21ee7575465a8ebba1ed9b0999e1b3aa46c785d1
Andualem <azemedkun@miu.edu>
2022-02-01 16:52:42 -0600

    Initial commit

---
+++ src/test/java/com/andy/FullStackReactSpringBootOne/FullStackReactSpringBootOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.FullStackReactSpringBootOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackReactSpringBootOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
